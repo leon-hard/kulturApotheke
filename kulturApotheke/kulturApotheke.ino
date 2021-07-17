03cd811bc85ba30c1aca945b9949a4ab73e59137
Probst <lprobst@ise.fhg.de>
2021-07-17 21:53:15 +0200

    Updated first test version

--- kulturApotheke/kulturApotheke.ino
+++ kulturApotheke/kulturApotheke.ino
@@ -1,14 +1,39 @@
+#include <ClickEncoder.h>
+
 #include "Arduino.h"
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_PN532.h>
+#include <ClickEncoder.h>
+#include <TimerOne.h>
 
+struct Tag 
+{
+  int byte_length;
+  int song_number;
+  bool continue_playing;
+  uint8_t id[7];
+};
+
+
+Tag tags[] = { {4, 1, 0, {0xD6,0xA0,0x1B,0xF8}}, 
+               {4, 2, 0, {0xDB,0x5E,0x43,0x0B}}, 
+               {7, 3, 1, {0x04,0x3F,0x78,0xC2,0x83,0x36,0x80}}, 
+             };
+
+/*
+Tag tags[] = { {4, 1, 0, 0}, 
+               {4, 2, 0, 1}, 
+             //  {7, 3, 1, {0x04,0x3F,0x78,0xC2,0x83,0x36,0x80}}, 
+             };
+*/
 // If using the breakout or shield with I2C, define just the pins connected
 // to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
 #define PN532_IRQ   (2)
 #define PN532_RESET (3)  // Not connected by default on the NFC Shield
+#define POWER_ON_OFF (9)
 const int DELAY_BETWEEN_CARDS = 100;
 long timeLastCardRead = 0;
 boolean readerDisabled = false;
@@ -22,12 +47,50 @@ SoftwareSerial mySoftwareSerial(11, 10); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
 void printDetail(uint8_t type, int value);
 uint16_t detectNFC(void);
-int volume = 10;
+int volume = 20;
 uint16_t isPlaying = 0;
+uint8_t keepPlaying;
 uint16_t counter = 0;
+uint16_t counter_shutdown = 0;
+uint16_t n_shutdown = 0;
+
+const int analogInPinButton = A3;  
+const int analogInPinBattery = A2;  // 480 = 3.8 V => 3.2 V = 404
+#define BATTERY_LOW 420
+#define BATTERY_LOW_HYSTERESIS  5
+int sensorValue = 0;        // value read from the pot
+
+int LEDblue = 6; 
+int LEDred = 4;
+int LEDgreen = 5; 
+
+
+int continue_playing = 0;
+int isFinished = 0;
+
+
+
+ClickEncoder *encoder;
+int16_t last, value;
+
+void timerIsr()
+{
+  encoder->service();
+}
+
 
 void setup()
 {
+  pinMode(POWER_ON_OFF, OUTPUT);
+  digitalWrite(POWER_ON_OFF, HIGH);
+
+  pinMode(LEDblue, OUTPUT);
+  digitalWrite(LEDblue, HIGH);
+  pinMode(LEDgreen, OUTPUT);
+  digitalWrite(LEDgreen, LOW);
+  pinMode(LEDred, OUTPUT);
+  digitalWrite(LEDred, HIGH);
+  
   mySoftwareSerial.begin(9600);
   Serial.begin(115200);
 
@@ -43,11 +106,11 @@ void setup()
   }
   Serial.println(F("DFPlayer Mini online."));
 
-  myDFPlayer.volume(10);  //Set volume value. From 0 to 30
+  myDFPlayer.volume(volume);  //Set volume value. From 0 to 30
   //myDFPlayer.play(1);  //Play the first mp3
 
 
-
+  
   nfc.begin();
 
   uint32_t versiondata = nfc.getFirmwareVersion();
@@ -69,23 +132,176 @@ void setup()
 
   
   //Serial.println("Waiting for an ISO14443A Card ...");
+
+  encoder = new ClickEncoder(A0, A1);
+
+  Timer1.initialize(1000);
+  Timer1.attachInterrupt(timerIsr);
+
+  last = -1;
+  
 }
 
 void loop()
 {
+
+  value = volume;
+  value += encoder->getValue();
+
+  if (value != last)
+  {
+    if (value > 30)
+    {
+      value = 30;
+    }
+    else if (value < 0)
+    {
+      value = 0;
+    }
+    last = value;
+    volume = last;
+    Serial.print("Encoder Value: ");
+    Serial.println(value);
+    myDFPlayer.volume(volume);
+  }
+  
   static unsigned long timer = millis();
+
+  static int counter_pause = 0;
+  static int counter_finished = 0;
 
   if (millis() - timer > 100) {
     timer = millis();
 
     if(counter++ > 1) {
-      counter = 0;
-      isPlaying = 0;
-      myDFPlayer.pause();
+
+      if(keepPlaying == 0) {
+        counter = 0;
+        isPlaying = 0;
+        digitalWrite(LEDblue, HIGH);
+        myDFPlayer.pause();
+      }
+    
+
+      if(continue_playing) {
+        if(counter_pause++ > 50) {
+          counter_pause = 0;
+          continue_playing = 0;
+        }
+      }
+
+      if(isFinished) {
+        if(counter_finished++ > 1) {
+          isFinished = 0;
+          //continue_playing = 0;
+          keepPlaying = 0;
+          counter_finished = 0;
+        }
+      }
+
+
     }
+
+
+
+    
     //startListeningToNFC();
     //myDFPlayer.next();  //Play next mp3 every x second.
+
+
+    if(counter_shutdown++ > 5) {
+      counter_shutdown = 0;
+
+      static uint8_t counter_lowpass_battery = 0;
+      static uint16_t sum_lowpass_battery = 0;
+
+
+      sum_lowpass_battery += analogRead(analogInPinBattery);
+
+      if(++counter_lowpass_battery >= 8) {
+        
+        int analogInBattery = sum_lowpass_battery >> 3;
+        sum_lowpass_battery = 0;
+        counter_lowpass_battery = 0;
+  
+        Serial.print("Battery\t");
+        Serial.println(analogInBattery);
+        //Serial.print(" | ");
+
+        if(analogInBattery < (BATTERY_LOW - BATTERY_LOW_HYSTERESIS)) {
+          digitalWrite(LEDred, LOW);
+        } else if(analogInBattery > (BATTERY_LOW + BATTERY_LOW_HYSTERESIS)) {
+          digitalWrite(LEDred, HIGH);
+        }
+      }
+      
+
+      static uint16_t LED_state = 0;
+      
+      if(LED_state == 0) {
+        
+        static uint16_t LED_counter = 0;
+
+        if(LED_counter++ >= 3) {
+            digitalWrite(LEDred, HIGH);
+            digitalWrite(LEDgreen, HIGH);
+            digitalWrite(LEDblue, HIGH);
+            LED_state = 1;
+        }
+
+
+      }
+      
+      sensorValue = analogRead(analogInPinButton);
+
+      //Serial.print("Switch: ");
+      //Serial.println(sensorValue);
+
+      if(sensorValue > 650) {
+        n_shutdown++;
+        digitalWrite(LEDred, LOW);
+      } else {
+        n_shutdown = 0;
+        //digitalWrite(LEDred, HIGH);
+      }
+
+      if(n_shutdown >= 3){
+
+        digitalWrite(LEDblue, HIGH);
+        digitalWrite(LEDgreen, HIGH);
+        digitalWrite(LEDred, HIGH);
+        digitalWrite(POWER_ON_OFF, LOW);
+      } else {
+
+
+
+        /*
+        switch(LED_counter) {
+          case 0:
+            digitalWrite(LEDblue, LOW);
+            digitalWrite(LEDgreen, HIGH);
+            digitalWrite(LEDred, HIGH);
+            break;
+          case 1:
+            digitalWrite(LEDblue, HIGH);
+            digitalWrite(LEDgreen, LOW);
+            digitalWrite(LEDred, HIGH);
+            break;
+          case 2:
+            digitalWrite(LEDblue, HIGH);
+            digitalWrite(LEDgreen, HIGH);
+            digitalWrite(LEDred, LOW);
+            break;
+        }
+  
+        if(++LED_counter > 2) {
+          LED_counter = 0;
+        }
+        */
+      }
+    }
   }
+  
 
   int x = 0;
   String str;
@@ -94,6 +310,7 @@ void loop()
   String play = "play";
   String pause = "pause";
   String start = "start";
+  String power = "power";
 
   if(Serial.available() > 0) {
     
@@ -120,6 +337,8 @@ void loop()
   } else if (str == start) {
     //myDFPlayer.volume(volume);
     myDFPlayer.start();
+  } else if (str == power) {
+    digitalWrite(POWER_ON_OFF, x);
   }
 
   if (myDFPlayer.available()) {
@@ -157,6 +376,7 @@ void loop()
 
 
   //detectNFC();
+  
 }
 
 void printDetail(uint8_t type, int value){
@@ -180,6 +400,13 @@ void printDetail(uint8_t type, int value){
       Serial.print(F("Number:"));
       Serial.print(value);
       Serial.println(F(" Play Finished!"));
+
+      //continue_playing = 0;
+      //isPlaying = 0;
+      isFinished = 1;
+      keepPlaying = 0;
+      digitalWrite(LEDblue, HIGH);
+      
       break;
     case DFPlayerError:
       Serial.print(F("DFPlayerError:"));
@@ -212,6 +439,11 @@ void printDetail(uint8_t type, int value){
     default:
       break;
   }
+
+
+
+  
+
 }
 
 void startListeningToNFC() {
@@ -240,18 +472,63 @@ void handleCardDetected() {
 
       
       counter = 0;
-          
+
+      for(Tag &tag:tags) {
+
+        if(tag.byte_length == 4) {
+          if(uid[0] == tag.id[0] && uid[1] == tag.id[1] && uid[2] == tag.id[2] && uid[3] == tag.id[3]) {
+          //if(uid[0] == tag.id) {
+            if(isPlaying != tag.song_number) {
+              isPlaying = tag.song_number;
+              keepPlaying = tag.continue_playing;
+              if(!isFinished) {
+                
+                digitalWrite(LEDblue, LOW);
+                if(!continue_playing) {
+                  continue_playing = 1;
+                  myDFPlayer.play(isPlaying);
+                } else {
+                  myDFPlayer.start();
+                }
+              }
+
+            }
+          }
+        } else if(tag.byte_length == 7) {
+          if(uid[0] == tag.id[0] && uid[1] == tag.id[1] && uid[2] == tag.id[2] && uid[3] == tag.id[3] && uid[4] == tag.id[4] && uid[5] == tag.id[5] && uid[6] == tag.id[6]) {
+            if(isPlaying != tag.song_number) {
+              isPlaying = tag.song_number;
+              keepPlaying = tag.continue_playing;
+              if(!isFinished) {
+                
+                digitalWrite(LEDblue, LOW);
+                if(!continue_playing) {
+                  continue_playing = 1;
+                  myDFPlayer.play(isPlaying);
+                } else {
+                  myDFPlayer.start();
+                }
+              }
+            }
+          }
+        }
+      }
+
+
+      /*
       if (uidLength == 4) {
 
         if(uid[0] == 0xD6 && uid[1] == 0xA0 && uid[2] == 0x1B && uid[3] == 0xF8) {
           if(isPlaying != 3) {
             isPlaying = 3;
+            digitalWrite(LEDblue, LOW);
             myDFPlayer.play(isPlaying);
           }
         }
         else if(uid[0] == 0xDB && uid[1] == 0x5E && uid[2] == 0x43 && uid[3] == 0x0B) {
-          if(isPlaying != 10) {
-            isPlaying = 10;
+          if(isPlaying != 1) {
+            isPlaying = 1;
+            digitalWrite(LEDblue, LOW);
             myDFPlayer.play(isPlaying);
           }
         }
@@ -259,10 +536,13 @@ void handleCardDetected() {
         if(uid[0] == 0x04 && uid[1] == 0x3F && uid[2] == 0x78 && uid[3] == 0xC2 && uid[4] == 0x83 && uid[5] == 0x36 && uid[6] == 0x80) {
           if(isPlaying != 2) {
             isPlaying = 2;
+            digitalWrite(LEDblue, LOW);
             myDFPlayer.play(isPlaying);
           }
         }
       }
+
+      */
 
       
 
@@ -282,11 +562,12 @@ void handleCardDetected() {
         Serial.println(cardid);
       }
       Serial.println("");
-
       */
 
       timeLastCardRead = millis();
     }
+
+
 
     // The reader will be enabled again after DELAY_BETWEEN_CARDS ms will pass.
     readerDisabled = true;
